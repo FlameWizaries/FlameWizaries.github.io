943f95388d674be08b19d869f27291d957d7ed56
FlameWizaries <68864100+FlameWizaries@users.noreply.github.com>
2025-10-10 12:13:01 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
+const currentPage = window.location.pathname.split("/").pop();
+const menuLinks = document.querySelectorAll('.nav-item a');
+
+menuLinks.forEach(link => {
+    const linkPage = link.getAttribute('href').split("/").pop();
+    if (linkPage === currentPage) {
+        link.classList.add('active')
+    }
+});
+
+
+
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     nav.classList.toggle('active');
+});
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        burger.classList.remove('active');
+        nav.classList.remove('active');
+    });
 });
